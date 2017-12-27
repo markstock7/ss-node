65f96c53d116e8db425e4fe1b257f13e0adbfa46
mark stock <markstock7@hotmail.com>
2017-12-27 21:21:58 +0800

    a

--- js-src/data/schema/tables.js
+++ js-src/data/schema/tables.js
@@ -4,7 +4,6 @@ module.exports = {
         password: { type: 'string', nullable: false },
     },
     flows: {
-        id: { type: 'string', maxlength: 24, nullable: false, primary: true },
         port: { type: 'integer', maxlength: 60, nullable: false, defaultTo: 0 },
         flow: { type: 'bigInteger', defaultTo: 0 },
         time: { type: 'bigInteger', nullable: false }
